23a0c2134ccb63cfb0505cd5d73ae16010c73487
Riju Pramanik <pramanikriju@gmail.com>
2021-06-06 00:22:47 +0530

    landing page tweaks and image fixes

--- pages/index.js
+++ pages/index.js
@@ -35,20 +35,19 @@ export default function Example() {
         <meta name="theme-color" content="#5b86e5" />
       </Head>
       <Navbar />
-      <main className="mt-6 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:my-24 lg:px-8 xl:mt-26 ">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-10">
+      <main className="mt-6 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:my-24 lg:px-8 xl:mt-26  ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-10 ">
           <div className="">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
-                <span className="block main-font xl:inline ">
+                <div className="block main-font headline leading-tight">
                   Give your business a
-                </span>
-                <br className="hidden sm:inline" />
-                <span className="block main-text xl:inline">
+                </div>
+                <div className="block main-text headline leading-tight md:leading-none">
                   technology boost
-                </span>
+                </div>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 I'm Riju Pramanik, a full stack developer and technical
                 consultant. This site is still under construction, stay tuned
                 for updates!
@@ -86,7 +85,7 @@ export default function Example() {
             <div className="block max-w-48 md:w-48 md:h-48 rounded-full  mx-auto md:-mt-24 w-48 h-48 -mt-24 border-4 border-primary-blue">
               <Image
                 className="block max-w-48 md:w-48 md:h-48 rounded-full mx-auto md:-mt-24 w-32 h-32 -mt-16 "
-                src="/img/me.webp"
+                src="/img/profile.jpg"
                 width={48}
                 height={48}
                 layout="responsive"
